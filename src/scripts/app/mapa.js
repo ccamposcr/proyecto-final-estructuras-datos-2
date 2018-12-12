0cb2f1f00dc2bfa5f75e010c5c4c0a9fce3409e1
CHRISTIANPC\Christian <ccamposcr@gmail.com>
2018-12-11 22:36:24 -0600

    Update mapa.js

--- src/scripts/app/mapa.js
+++ src/scripts/app/mapa.js
@@ -5,18 +5,20 @@ define([
 ], function(datos, hashing) {
     let mapa = {};
     let matrizAdyacencia = [
-        [0, 1, 0,1],
+        [0, 1, 0, 1],
         [1, 0, 1, 0],
         [0, 1, 0, 1],
         [1, 0, 1, 0]
     ];
 
-    let matrizDeCostos = [
-        [0, 4, 0,6],
+    let matrizDePesos = [
+        [0, 4, 0, 6],
         [4, 0, 6, 0],
         [0, 6, 0, 8],
         [6, 0, 8, 0]
     ];
+
+    let caminos = [];
     //[0,1,2,3,4,5,6,7,8,9];
     let vertices = ['LIM','CMX','SCL','AQP'];
     //https://cdn-images-1.medium.com/max/1600/1*K_dtpNyaJ41uOEW_mHvW4A.png
@@ -31,8 +33,51 @@ define([
         let verticeDestino = vertices.findIndex(k => k== destino);
         console.log(verticeOrigen);
         console.log(verticeDestino);
+
+     
+        var k;
+        var c = '';
+        k = p[i][j];
+        if (k==0)
+            return;
+        caminos(i,k);
+        cout<<k;
+        cout<<c;
+        getch();
+        caminos(k,j);
+        
     };
+
+    let calcularCaminos = () => {
+        let i, j, k;
+        var array = [];
+
+        for (i = 1; i <= array.length; i++){
+            for (j = 1; j <= 7; j++){
+                array[i][j] = matrizDePesos[i][j];
+                caminos[i][j] = 0;
+            }             
+        } 
+
+        for (i = 1; i <= array.length; i++){
+            array[i][i] = 0;
+        } 
+
+        for (k = 1;k <= array.length; k++){
+            for (i = 1;i <= array.length; i++){
+                for (j = 1; j <= array.length; j++){
+                    if ((array[i][k] + array[k][j]) < (array[i][j]))
+                    {
+                        array[i][j] = array[i][k] + array[k][j];
+                        caminos[i][j] = k;
+                    }
+                }
+            }
+        }
+    };
+
     let buscarCaminoMaximo = () => {};
 
-    buscarCaminoMinimo('LIM','CMX');
+    calcularCaminos();
+    buscarCaminoMinimo('LIM','AQP');
 });
