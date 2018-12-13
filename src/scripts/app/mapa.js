2002213947d05b800ce0dcdad8675da94c35af25
CHRISTIANPC\Christian <ccamposcr@gmail.com>
2018-12-12 23:13:53 -0600

    Update mapa.js

--- src/scripts/app/mapa.js
+++ src/scripts/app/mapa.js
@@ -18,11 +18,13 @@ define([
     ];
 	
     let matrizDePesos = [
-        [0, 4, 0, 6],
-        [4, 0, 6, 0],
-        [0, 6, 0, 8],
-        [6, 0, 8, 0]
+        [0, 4, Infinity, 6],
+        [4, 0, 6, Infinity],
+        [Infinity, 6, 0, 8],
+        [6, Infinity, 8, 0]
     ];
+
+    let caminosMinimos = [];
     
     let initMatriz = () => {
         let i, j, matriz = [];
@@ -75,6 +77,8 @@ define([
     let buscarCaminoMinimo = (origen, destino) => {
         let verticeOrigen = vertices.findIndex(k => k == origen);
         let verticeDestino = vertices.findIndex(k => k == destino);
+        console.log(verticeOrigen);
+        console.log(verticeDestino);
         if( verticeOrigen >= 0 &&  verticeDestino >= 0){
             buscarCaminoMinRecursivo(verticeOrigen, verticeDestino);
         }
@@ -95,34 +99,6 @@ define([
         buscarCaminoMinRecursivo(k, destino);
     };
 
-    let calcularCaminos = () => {
-        let i, j, k;
-        let matrizTmp = initMatriz();
-
-        for (i = 0; i < matrizTmp.length; i++){
-            for (j = 0; j < matrizTmp.length; j++){
-                matrizTmp[i][j] = matrizDePesos[i][j];
-                caminos[i][j] = 0;
-            }             
-        } 
-
-        for (i = 0; i < matrizTmp.length; i++){
-            matrizTmp[i][i] = 0;
-        } 
-
-        for (k = 0; k < matrizTmp.length; k++){
-            for (i = 0; i < matrizTmp.length; i++){
-                for (j = 0; j < matrizTmp.length; j++){
-                    if ((matrizTmp[i][k] + matrizTmp[k][j]) < (matrizTmp[i][j]))
-                    {
-                        matrizTmp[i][j] = matrizTmp[i][k] + matrizTmp[k][j];
-                        caminos[i][j] = k;
-                    }
-                }
-            }
-        }
-        console.log(caminos);
-    };
 
     let buscarCaminoMaximo = () => {};
 	
@@ -140,8 +116,9 @@ define([
 
     mapa.iniciarMapa = () => {
         //iniciarMapa();
-        calcularCaminos();
-        buscarCaminoMinimo('CMX','SCL');
+        //calcularCaminos();
+        //buscarCaminoMinimo('CMX','SCL');
+        caminosMinimos = floydWarshall(matrizDePesos);
     };
     
     //https://cdn-images-1.medium.com/max/1600/1*K_dtpNyaJ41uOEW_mHvW4A.png
@@ -151,6 +128,82 @@ define([
     mapa.buscarPorUbicacion = buscarUbicacion;
     mapa.buscarAdyacentes = buscarAdyacentes;
     mapa.datos = datos;
+    
+
+    var floydWarshall = (function () {
+        /**
+         * Matrix used for the algorithm.
+         */
+        var dist;
+        /**
+         * Initialize the distance matrix.
+         *
+         * @private
+         * @param {Array} graph Distance matrix of the array.
+         * @return {Array} Distance matrix used for the algorithm.
+         */
+        function init(graph) {
+          var dist = [];
+          var size = graph.length;
+          for (var i = 0; i < size; i += 1) {
+            dist[i] = [];
+            for (var j = 0; j < size; j += 1) {
+              if (i === j) {
+                dist[i][j] = 0;
+              } else if (!isFinite(graph[i][j])) {
+                dist[i][j] = Infinity;
+              } else {
+                dist[i][j] = graph[i][j];
+              }
+            }
+          }
+          return dist;
+        }
+        /**
+         * Floyd-Warshall algorithm. Finds the shortest path between
+         * each two vertices.<br><br>
+         * Complexity: O(|V|^3) where V is the number of vertices.
+         *
+         * @public
+         * @module graphs/shortest-path/floyd-warshall
+         * @param {Array} graph A distance matrix of the graph.
+         * @return {Array} Array which contains the shortest
+         *    distance between each two vertices.
+         *
+         * @example
+         * var floydWarshall =
+         * require('path-to-algorithms/src/graphs/shortest-path/floyd-warshall').floydWarshall;
+         * var distMatrix =
+         *    [[Infinity, 7,        9,       Infinity,  Infinity, 16],
+         *     [7,        Infinity, 10,       15,       Infinity, Infinity],
+         *     [9,        10,       Infinity, 11,       Infinity, 2],
+         *     [Infinity, 15,       11,       Infinity, 6,        Infinity],
+         *     [Infinity, Infinity, Infinity, 6,        Infinity, 9],
+         *     [16,       Infinity, 2,        Infinity, 9,        Infinity]];
+         *
+         * // [ [ 0, 7, 9, 20, 20, 11 ],
+         * //   [ 7, 0, 10, 15, 21, 12 ],
+         * //   [ 9, 10, 0, 11, 11, 2 ],
+         * //   [ 20, 15, 11, 0, 6, 13 ],
+         * //   [ 20, 21, 11, 6, 0, 9 ],
+         * //   [ 11, 12, 2, 13, 9, 0 ] ]
+         * var shortestDists = floydWarshall(distMatrix);
+         */
+        return function (graph) {
+          dist = init(graph);
+          var size = graph.length;
+          for (var k = 0; k < size; k += 1) {
+            for (var i = 0; i < size; i += 1) {
+              for (var j = 0; j < size; j += 1) {
+                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                  dist[i][j] = dist[i][k] + dist[k][j];
+                }
+              }
+            }
+          }
+          return dist;
+        };
+      }());
 	
 	
 	return mapa;
