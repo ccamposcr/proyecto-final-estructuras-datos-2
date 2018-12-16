61107e4462060bf5ee0f233d3eff71604b39f1bd
adriancr93 <aobregonr@ucenfotec.ac.cr>
2018-12-16 15:57:23 -0600

    New Key api google

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -10,7 +10,7 @@ requirejs.config({
         "hashing": "scripts/app/hashing",
         "apiGoogle": "scripts/app/apiGoogle",
         "algoritmos": "scripts/app/algoritmos",
-        "googleMaps": "https://maps.googleapis.com/maps/api/js?key=AIzaSyAYDiiPeMSVqarqP8kPOnYpXTVM_2yC3TU",
+        "googleMaps": "https://maps.googleapis.com/maps/api/js?key=AIzaSyAVTOu-bHLsE5PNp1NlVMDQsycnZ1ALlps",
         "jquery": "https://code.jquery.com/jquery-3.3.1.slim.min.js",
         "popper": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
         "bootstrap": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
