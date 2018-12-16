20ec3d8c9f70de00e3507320e958e26e3667d5fd
adriancr93 <aobregonr@ucenfotec.ac.cr>
2018-12-16 15:34:50 -0600

    Update Hashing

--- src/scripts/app/hashing.js
+++ src/scripts/app/hashing.js
@@ -1,8 +1,8 @@
 define(['datos'], () => {
-    
+
     let hash = {};
 
-    String.prototype.hashCode = function () {
+    String.prototype.hashCode = function (max) {
         var hash = 0;
         if (!this.length) return hash;
         for (i = 0; i < this.length; i++) {
@@ -13,10 +13,39 @@ define(['datos'], () => {
         return Math.abs(max ? hash % max : hash);
     };
 
+    class CeldaTabla {
+        constructor(clave, ubicaciones) {
+            this.clave = clave;
+            this.ubicaciones = ubicaciones;
+        }
+    }
+
     class hashing {
+        constructor(ubicaciones, nombreIdentificador) {
+            this.table = [];
+            this.crearTabla(ubicaciones);
+            this.nombreIdentificador = nombreIdentificador;
+            this.crearTabla(ubicaciones);
+        }
+    
+        crearTabla(ubicaciones) {
+            var value = 0;
+            for (var i = 0; i < ubicaciones.length; i++) {
+                ubicaciones[i] = value;
+            }
+            this.tabla = this.tabla.fill.call({ length: this.value }, []);
+            for (const ubicaciones of ubicaciones) {
+                this.insertar(ubicaciones);
+            }
+        }
 
-        crearTabla() {
-
+        codigoHash(clave) {
+            switch (typeof clave) {
+                case 'string':
+                    return clave.hashCode(this.size);
+                default:
+                    return Math.abs(clave % this.size);
+            }
         }
 
         funcionaHash() {
@@ -24,15 +53,29 @@ define(['datos'], () => {
         }
 
         insertar() {
-
+            if (ubicaciones.hasOwnProperty(this.nombreIdentificador)) {
+                let hash = this.codigoHash(ubicaciones[this.nombreIdentificador]);
+                this.tabla[hash].push(new CeldaTabla(ubicaciones[this.nombreIdentificador], ubicaciones));
+            }
         }
 
-        obtener() {
+        obtener(clave) {
+           let index = this.codigoHash(clave);
+           let bucket = this.tabla[index];
+           let resultado = [];
+
+           for (let i = 0; i < bucket.length; i++) {
+               if (bucket[i].clave === clave) {
+                   resultado.push(bucket[i].ubicaciones);
+               }
+           }
+
+           return resultado;
 
         }
 
     }
-    
+
     return hash;
 
 });
